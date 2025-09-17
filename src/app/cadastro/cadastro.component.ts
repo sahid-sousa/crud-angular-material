a1bd17ba016f1a1aa6746f887b4c04b8721bb6d5
Sahid Sousa <sahid.sousa@bol.com.br>
2025-09-17 18:26:32 -0300

    Finalizado:  - Operacoes de CRUD

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { Cliente } from './cliente';
+import { ClienteService } from '../cliente.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -12,11 +17,50 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+
+  cliente: Cliente = Cliente.newCliente();
+  atualizando: boolean = false;
+
+
+  constructor(
+    private service: ClienteService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+
+  ngOnInit() : void {
+    this.route.queryParamMap.subscribe( (query: any) => {
+      const params = query['params'];
+      const id = params['id'];
+      if (id) {
+        let clienteEncontrado = this.service.buscarClientePorId(id);
+        if (clienteEncontrado) {
+          this.atualizando = true;
+          this.cliente = clienteEncontrado;
+        }
+      }
+    });
+  }
+
+  salvar() {
+    if (!this.atualizando) {
+      this.service.salvar(this.cliente);
+      this.cliente = Cliente.newCliente();
+    } else {
+      this.service.atualizar(this.cliente);
+      this.router.navigate(["/consulta"]).then(r =>
+        console.log(r)
+      );
+    }
+  }
 
 }
--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatIconModule } from '@angular/material/icon'
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import {ClienteService} from '../cliente.service';
+import {Cliente} from '../cadastro/cliente';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatTableModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatTableModule,
+    MatButtonModule
+  ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit {
+
+  nomeBusca: string = '';
+  listaClientes: Cliente[] = [];
+  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email", "acoes"];
+  deletando: boolean = false;
+
+  constructor(
+    private service: ClienteService,
+    private router: Router
+  ) {}
+
+  ngOnInit() : void {
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
+
+  pesquisar() {
+    this.listaClientes = this.service.pesquisarClientes(this.nomeBusca);
+  }
+
+  preparaEditar(id: string) {
+    this.router.navigate(['/cadastro'], {queryParams: {"id": id}}).then(r =>
+      console.log(r)
+    )
+  }
+
+  preparaDeletar(cliente: Cliente) {
+    cliente.deletando = true;
+  }
+
+  deletar(cliente: Cliente) {
+    this.service.deletar(cliente);
+    this.listaClientes = this.service.pesquisarClientes('');
+    this.deletando = false;
+  }
+
 
 }
